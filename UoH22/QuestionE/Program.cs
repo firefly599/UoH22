68a94aef3cf00b4db5d5423f2f808e911730754e
Louie <louie.waterfall@hotmail.co.uk>
2022-03-26 15:10:55 +0000

    Stuff

---
+++ UoH22/QuestionE/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuestionE
+{
+    class Program
+    {
+        private static List<int> ReadFilms()
+        {
+            List<int> films = new List<int>();
+            string[] numbers = Console.ReadLine().Split(" ");
+            for(int i = 0; i < int.Parse(numbers[0]); i++)
+            {
+                films.Add(int.Parse(numbers[i+1]));
+            }
+
+            return films;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> filmDays1 = ReadFilms();
+            List<int> filmDays2 = ReadFilms();
+
+            filmDays1.Sort();
+            filmDays2.Sort();
+
+            int totalFilms = 0;
+            int lastWatchedFilm = -1;
+            bool person1LikedLast = false;
+            bool person2LikedLast = true;
+            for(int i = 0; i < Math.Max(filmDays1.Count, filmDays2.Count); i++)
+            {
+
+            }
+        }
+    }
+}
