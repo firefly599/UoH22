49c9ebe1124a922049b31cd6f5c48cafeb51d9cc
Louie <louie.waterfall@hotmail.co.uk>
2022-03-26 17:13:02 +0000

    Me stupid

--- UoH22/QuestionE/Program.cs
+++ UoH22/QuestionE/Program.cs
@@ -17,22 +17,68 @@ namespace QuestionE
             return films;
         }
 
+        struct filmInfo
+        {
+            public bool person1Liked;
+            public bool person2Liked;
+        }
+
         static void Main(string[] args)
         {
             List<int> filmDays1 = ReadFilms();
             List<int> filmDays2 = ReadFilms();
 
-            filmDays1.Sort();
-            filmDays2.Sort();
+            Dictionary<int, filmInfo> films = new Dictionary<int, filmInfo>();
+            for (int i = 0; i < filmDays1.Count; i++)
+            {
+                films[filmDays1[i]] = new filmInfo()
+                {
+                    person1Liked = true
+                };
+            }            
+            
+            for (int i = 0; i < filmDays2.Count; i++)
+            {
+                int day = filmDays2[i];
+                films[day] = new filmInfo()
+                {
+                    person1Liked = films.ContainsKey(day),
+                    person2Liked = true
+                };
+            }
+
+            List<int> days = new List<int>();
+            foreach(var kvp in films)
+            {
+                days.Add(kvp.Key);
+            }
+
+            days.Sort();
 
             int totalFilms = 0;
-            int lastWatchedFilm = -1;
-            bool person1LikedLast = false;
+            bool person1LikedLast = true;
             bool person2LikedLast = true;
-            for(int i = 0; i < Math.Max(filmDays1.Count, filmDays2.Count); i++)
+            for(int i = 0; i < days.Count; i++)
             {
+                filmInfo info = films[days[i]];
 
+                if(info.person1Liked && info.person2Liked)
+                {
+                    totalFilms++;
+                    person1LikedLast = true;
+                    person2LikedLast = true;
+                    continue;
+                }
+
+                if ((!info.person1Liked && !person1LikedLast) || (!info.person2Liked && !person2LikedLast)) break; // I'm an idiot
+
+                person1LikedLast = info.person1Liked;
+                person2LikedLast = info.person2Liked;
+
+                totalFilms++;
             }
+
+            Console.WriteLine(totalFilms);
         }
     }
 }
